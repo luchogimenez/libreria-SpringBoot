d8345fdfb318c8f016745993e37feea9c6415891
luchogimenez <gimenezluis001@gmail.com>
2021-11-04 12:21:04 -0300

    merge

--- src/main/java/egg/libreria/libreria/entidades/Prestamo.java
+++ src/main/java/egg/libreria/libreria/entidades/Prestamo.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
